ecd373737baa3745d5f325484284782cf09bdc36
Software Laptop <4787robotics@gmail.com>
2016-03-29 17:29:02 -0500

    Cleaned up some comments

--- src/org/usfirst/frc/team4787/robot/Robot.java
+++ src/org/usfirst/frc/team4787/robot/Robot.java
@@ -19,23 +19,6 @@ import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.Image;
 import com.ni.vision.NIVision.ImageType;
 
-/**
- * This is a demo program showing the use of the RobotDrive class. The
- * SampleRobot class is the base of a robot application that will automatically
- * call your Autonomous and OperatorControl methods at the right time as
- * controlled by the switches on the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
 public class Robot extends SampleRobot {
 
 	// CANs, PWMs, USB slots, button config
